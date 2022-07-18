af9f7b63bdbca8f34b1d400418108439fddba603
21b01a0556 <109472375+21b01a0556@users.noreply.github.com>
2022-07-18 21:19:04 +0530

    Add files via upload

--- 6th question.py
+++ 6th question.py
@@ -1,7 +1,8 @@
-
+#6th question a)
 from array import *
-a=array('i',[1,4,2,6,8,3,10])      
-ele=int(input("Enter search element"))
+a=array('i',[1,4,2,6,8,3,10])  
+print(a)    
+ele=int(input("Enter search element:\t"))
 flag=0
 for i in range(len(a)):
         if(a[i] == ele):
@@ -13,3 +14,24 @@ if(flag == 0):
     print("Not found")
 else:
     print('Element found in',flag,'position')
+    
+#6th question b)
+s=input("Enter a string:\t")
+l=0
+j=0
+m=0
+k=0
+for i in range(len(s)):
+    if(s[i].isupper()):
+        l=l+1
+    elif(s[i].islower()):
+        j=j+1
+    elif(s[i].isnumeric()):
+        m=m+1
+    else:
+        k=k+1
+     
+print('No of uppercase letters: ',l)
+print('No of lowercase letters: ',j)
+print('No of numericals: ',m)
+print('No of special characters',k)
