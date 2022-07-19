3e6a581ffda8514f4fb1b0f4f6851a57ebc6bc80
21b01a0556 <109472375+21b01a0556@users.noreply.github.com>
2022-07-19 21:07:57 +0530

    Update 5th question Test module.py

--- 5th question Test module.py
+++ 5th question Test module.py
@@ -1,11 +1,33 @@
 #Test.py
 import Prime
 from Prime import *
-print("The first 50 prime numbers are:")
+print("The first 100 prime numbers are:")
 k = 1
 i = 2
 while k<100:
 	if isPrime(i):
+		k+=1
+		print(i,end =" ")
+		i+=1
+	else:
+		i+=1
+
+print("The first 100 Emirp numbers are:")
+k = 1
+i = 2
+while k<100:
+	if isEmirp(i):
+		k+=1
+		print(i,end =" ")
+		i+=1
+	else:
+		i+=1
+
+print("The first 100 Palindrome Prime numbers are:")
+k = 1
+i = 2
+while k<100:
+	if isPalindromePrime(i):
 		k+=1
 		print(i,end =" ")
 		i+=1
@@ -22,10 +44,10 @@ for c in range(2,101):
 	if isEmirp(c):
 		print(c,end = " ")
 	
-print("\nMersenne numbers upto 100 are:")
+print("\nMersenne numbers upto 1000 are:")
 for c in range(2,33):
 	if mersennePrime(c):
 		print(c,end = " ")
 	
-print("\nTwin prime numbers below 100")
-printTwinPrime(200)	
+print("\nTwin prime numbers below 1000")
+printTwinPrime(1000)	
